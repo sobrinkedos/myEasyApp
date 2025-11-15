791d7796590061038749922b268f2b057ddd5103
Rilton Oliveira de Souza <riltons@bb.com.br>
2025-11-15 09:14:38 -0300

    fix: ajustar configuração do backend para Vercel serverless

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
-import 'dotenv/config';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import app from '../src/app';
 
 // Export the Express app as a Vercel serverless function
-export default app;
+export default async (req: VercelRequest, res: VercelResponse) => {
+  return app(req, res);
+};
